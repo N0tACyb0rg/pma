8adc1fa99e4e2bea3d69d441abde085e9bc1411b
N0tACyb0rg <n0tacyb0rg@proton.me>
2026-01-01 11:43:46 -0800

    Add initial authorization method.

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from fastapi import FastAPI, Request
-from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.post('/pf_auth')
-async def get_user(request: Request):
+@app.post('/pf_authen')
+async def get_authen(request: Request):
     body = await request.body()
 
     try:
@@ -17,3 +16,15 @@ async def get_user(request: Request):
 
     return {"result": 1, "message": "Successfully sent authorization request"}
 
+@app.post('/pf_author')
+async def get_author(request: Request):
+    body = await request.body()
+    try:
+        json_data = request.json()
+        print("Auth JSON:", json_data)
+    except Exception:
+        print("Raw Request:", body)
+
+    print("Headers:", request.headers)
+
+    return {"access_duration":"1H","access_level":"guest","sponsor":1 ,"unregdate":"2030-01-01","category":"default"}
